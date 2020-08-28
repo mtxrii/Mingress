85d7cdf9a953508989d74a7f046451598ddc4d27
Enrique <edavalos@ucsc.edu>
2020-08-28 04:30:14 -0700

    Added support for private keys

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import {keys} from './keys'
+
+Vue.prototype.$keyVars = keys;
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
