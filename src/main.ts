6f6d87eea7eee65e84354486d1ffdd3cde1e7348
Enrique <edavalos@ucsc.edu>
2020-08-28 05:46:41 -0700

    Imported VueMaterial

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
 import {keys} from './keys'
 
 Vue.prototype.$keyVars = keys;
