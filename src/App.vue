eb30316e2b8f4f4636f0df156e00be5a0dcf1ef9
Enrique <edavalos@ucsc.edu>
2020-08-28 06:50:59 -0700

    Added main page skeleton structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <div class="header">
+      <h2>Mingress</h2>
+    </div>
+
+    <div class="sidenav">
+      <div style="padding-top: 80px"> </div>
+      <md-button class="md-primary">Main Dash</md-button>
+      <md-button class="md-primary">All Products</md-button>
+      <md-button class="md-primary">Search</md-button>
+    </div>
+
+    <div class="content">
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </div>
   </div>
 </template>
 
@@ -26,4 +39,33 @@ export default Vue.extend({
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.header {
+  padding: 10px 20px;
+  background: #555;
+  color: #f1f1f1;
+  position: fixed;
+  text-align: left;
+  top: 0;
+  z-index: 1;
+  width: 100%;
+}
+
+.sidenav {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.content {
+  margin-left: 160px;
+  padding: 25px 10px;
+}
+
 </style>
