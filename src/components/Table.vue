c1b1240bd7fb876176b850621cbaaa72bc5bd8c6
Enrique <edavalos@ucsc.edu>
2020-09-02 02:24:18 -0700

    Finalized table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
+    <md-table v-model="products" md-sort="id" md-sort-order="desc" md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">All Products</h1>
+      </md-table-toolbar>
 
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="ID" md-sort-by="id">{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Price" md-sort-by="price">${{ item.price }}</md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
@@ -9,10 +19,20 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Table',
+  data: function() {
+    return {
+      products: [
+        {id: 0, name: "thing1", price: 10},
+        {id: 1, name: "object2", price: 50},
+        {id: 2, name: "item3", price: 20},
+      ]
+    }
+  },
+
+
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
